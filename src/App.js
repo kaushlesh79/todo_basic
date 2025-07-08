ca91d288bcecd67c7ff9ba579d6339916f1fe656
kaushlesh79 <kaushleshchaurasiya196@gmail.com>
2025-07-08 20:56:37 +0530

    Added the completed check through toggle fn; task is now an object

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@
 
 import { useState } from "react";
 
-//let tasklist = [];
+//let tasklist = [{text:"---" , completed : false}];
 
 
 
 function App() {
    
-  const [task , setTask] = useState("");  // ye input ko handle krta hai jo user likh rha wo value settask mei jayega 
+  const [task , setTask] = useState({text : "" , completed : false});  // ye input ko handle krta hai jo user likh rha wo value settask mei jayega 
+  // now task ab object bn chuka hai ab state mei text aur completed dono rhengi 
 
   const [tasklist , setTasklist]=  useState([]); // ye array aur ui ko handle krta hai arr mei value add krna agar valid input hai to 
 
   function AddTask(){
    
-    if(task.trim() !== ""){  //  ye function string ke leading/trailing spaces hata deta hai aur agar kuch nhi bcha to add hi nhi hoga
+    if(task.text.trim() !== ""){  //  ye function string ke leading/trailing spaces hata deta hai aur agar kuch nhi bcha to add hi nhi hoga
 
       setTasklist([...tasklist , task]);// adding current entered task to the tasklist
-      setTask(""); // input ko add task krne ke bad empty krdo
+      setTask({text :"" , completed : false}); // input ko add task krne ke bad empty krdo 
+      // as task abject hai to text ko empty krdo completed to hmesha false rhega till delete na kre 
 
     }
   
@@ -30,19 +32,50 @@ function App() {
     
     // whi element rkho jinka index idx_to_delet ke equal nhi hai naye wale list mei  //updatedlist 
     // phir setTasklist ko nayi list pass krdo taki ui pe re render ho jaye list 
-   const updatedlist = tasklist.filter((_, idx) => idx !==idx_to_delete);
-   setTasklist(updatedlist);
+  
+    const updatedlist=tasklist.filter((_, idx) => idx !==idx_to_delete );
+    setTasklist(updatedlist);
 
+
+  }
+
+  // toggle fn for chenging button as toogle btn tick and cross 
+
+  // if index equal to idx now change the completed of that tas to false 
+
+  function togglefn(index){
+    const updatedlist = tasklist.map((t , idx) => idx===index ? {...t , completed : !t.completed} : t);
+    setTasklist(updatedlist); // it will rerender the ui and do the changes as state is change for redux 
   }
 
 // rendering of array of tasks == tasklist 
 // also hr ek task ke aage delet button hai uspe click krenge index delettask fn ko jayega nya array bnayega us index wale ko filter out kr dega aur nya array rerender krwa dega settasklist(new_list) se 
-  const tasks = tasklist.map((curr_task , idx) => (<li key = {idx}>{curr_task}
-  <button style = {{marginLeft : "10px" , color : "red"}} onClick={() => deleteTask(idx)}>
-     ❌</button>
-     
-  </li>
-  ));
+ 
+//tasklist ui pe render krne ke liye 
+
+const tasks = tasklist.map((curr_task , idx) => (<li
+
+key = {idx}
+style = {{
+  display : "flex" , 
+  justifyItems : "center" ,
+  alignItems : "center" ,
+  gap : "10px" ,
+  marginBottom : "10px"
+}}>
+  <input type ="checkbox" checked = {curr_task.completed} onChange={() => togglefn(idx)}></input>
+
+  <span 
+  style = {{
+    textDecoration : curr_task.completed ? "line-through" : "none" ,
+    color :curr_task.completed ? "green" : "black" , 
+
+  }}
+  >
+    {curr_task.text}
+  </span>
+  <button onClick={() => deleteTask(idx)} style = {{color : "red"}}> ❌ </button>
+</li> ));
 
 
 
@@ -57,12 +90,14 @@ function App() {
     gap : "20px",
    }}>
 
-    <input type="text" placeholder="Enter the task you want to do" value = {task} onChange={(e) => setTask(e.target.value)}></input>
+    {/* // as ab task ek object hai to hmari value task.text rhegi  aur settask se task ya state me task.text ki value ko input mei show kr rhe hai  */}
+    
+    <input type="text" placeholder="Enter the task you want to do" value = {task.text} onChange={(e) => setTask({...task ,  text:e.target.value})}></input>
     <button onClick={AddTask}> Add task</button>
 
-
+   
     <h2>Tasklist</h2>
-    <u1>{tasks}</u1>
+    <ul style ={{listStyle : "none" , padding : 0 }}>{tasks}</ul>
    </div>
 
 
