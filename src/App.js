13cdffd1ec97d1c1d6b2783d5e6ebf117c8acc25
kaushlesh79 <kaushleshchaurasiya196@gmail.com>
2025-07-08 17:52:38 +0530

    This is a basic todo app. Till now I have just added: add task button, delete task button the future task are to add toggle deletebutton and tasks in local storage .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// two fn ek add ek delete 
+
+// usestate for rerendering 
+
+import { useState } from "react";
+
+//let tasklist = [];
+
+
 
 function App() {
+   
+  const [task , setTask] = useState("");  // ye input ko handle krta hai jo user likh rha wo value settask mei jayega 
+
+  const [tasklist , setTasklist]=  useState([]); // ye array aur ui ko handle krta hai arr mei value add krna agar valid input hai to 
+
+  function AddTask(){
+   
+    if(task.trim() !== ""){  //  ye function string ke leading/trailing spaces hata deta hai aur agar kuch nhi bcha to add hi nhi hoga
+
+      setTasklist([...tasklist , task]);// adding current entered task to the tasklist
+      setTask(""); // input ko add task krne ke bad empty krdo
+
+    }
+  
+
+  }
+
+  function deleteTask(idx_to_delete){
+    
+    // whi element rkho jinka index idx_to_delet ke equal nhi hai naye wale list mei  //updatedlist 
+    // phir setTasklist ko nayi list pass krdo taki ui pe re render ho jaye list 
+   const updatedlist = tasklist.filter((_, idx) => idx !==idx_to_delete);
+   setTasklist(updatedlist);
+
+  }
+
+// rendering of array of tasks == tasklist 
+// also hr ek task ke aage delet button hai uspe click krenge index delettask fn ko jayega nya array bnayega us index wale ko filter out kr dega aur nya array rerender krwa dega settasklist(new_list) se 
+  const tasks = tasklist.map((curr_task , idx) => (<li key = {idx}>{curr_task}
+  <button style = {{marginLeft : "10px" , color : "red"}} onClick={() => deleteTask(idx)}>
+     ❌</button>
+     
+  </li>
+  ));
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{display:"flex" ,
+    justifyContent : "center" , 
+    alignItems:"center" ,
+    flexDirection:"column",
+    height:"100vh",
+    gap : "20px",
+   }}>
+
+    <input type="text" placeholder="Enter the task you want to do" value = {task} onChange={(e) => setTask(e.target.value)}></input>
+    <button onClick={AddTask}> Add task</button>
+
+
+    <h2>Tasklist</h2>
+    <u1>{tasks}</u1>
+   </div>
+
+
+
+
   );
 }
 
